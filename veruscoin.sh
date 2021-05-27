c8a212b4bd1236fc79767e32401a488ff3b40214
anmol90400 <81383551+anmol90400@users.noreply.github.com>
2021-05-27 09:36:41 +0100

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
+! tar -xf lolMiner_v1.28a_Lin64.tar.gz
+%cd 1.28a/
+!./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0x23D33261d73C8669E29307968Da30f1371bb3B53
+.colab --ethstratum ETHPROXY
